0fdd58223c298914690101a314243220f63c276e
Johannes Oster <johannes.oster@outlook.at>
2020-06-04 14:27:01 +0200

    Add dev and test env db urls

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,6 +4,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pgp = pgPromise({capSQL: true});
-const db = pgp(process.env.DB_URL || 'clear');
+
+const dbURL = () => {
+  switch (process.env.NODE_ENV) {
+    case 'testing':
+      return process.env.TEST_DB_URL;
+    default:
+      return process.env.DEV_DB_URL;
+  }
+};
+const db = pgp(dbURL() || '');
 
 export default db;
