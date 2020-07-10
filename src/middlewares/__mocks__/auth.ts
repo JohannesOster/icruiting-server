585789caa0b41c4cd387e22fa018c113e40e7fe5
Johannes Oster <johannes.oster@outlook.at>
2020-07-10 15:45:19 +0200

    Add userRole to requireAuth mock

--- src/middlewares/__mocks__/auth.ts
+++ src/middlewares/__mocks__/auth.ts
@@ -4,6 +4,7 @@ export const requireAuth: RequestHandler = (req, res, next) => {
   res.locals.user = {
     orgID: process.env.TEST_ORG_ID,
     sub: process.env.TEST_USER_ID,
+    userRole: 'admin',
   };
   next();
 };
