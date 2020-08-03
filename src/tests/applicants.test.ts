786ba76ef161953a08443c0a1b2b872111d1b69f
Johannes Oster <johannes.oster@outlook.at>
2020-08-03 19:09:39 +0200

    Remove border from submit button in html form

--- src/tests/applicants.test.ts
+++ src/tests/applicants.test.ts
@@ -7,23 +7,31 @@ import {random} from 'faker';
 import {TApplicant} from 'controllers/applicants';
 import {insertForm} from '../db/forms.db';
 import {insertScreening} from '../db/screenings.db';
+import faker from 'faker';
 
 const mockUser = fake.user();
 jest.mock('../middlewares/auth', () => ({
+  requireAdmin: jest.fn(),
   requireAuth: jest.fn((_, res, next) => {
     res.locals.user = mockUser;
     next();
   }),
-  requireAdmin: jest.fn(),
 }));
 
-const insert = db.$config.pgp.helpers.insert;
-
 let jobIds: string[];
-beforeAll(async (done) => {
+
+/* HELPER FUNCTIONS */
+const insert = db.$config.pgp.helpers.insert;
+const randomJobId = () => {
+  const randomIdx = random.number({min: 0, max: jobIds.length - 1});
+  const randomJobId = jobIds[randomIdx];
+  return randomJobId;
+};
+
+beforeAll(async () => {
   // insert organization
-  const organization = fake.organization(mockUser.orgID);
-  const orgStmt = insert(organization, null, 'organization');
+  const fakeOrg = fake.organization(mockUser.orgID);
+  const orgStmt = insert(fakeOrg, null, 'organization');
   await db.none(orgStmt);
 
   // insert jobs
@@ -32,17 +40,16 @@ beforeAll(async (done) => {
     fake.job(mockUser.orgID),
     fake.job(mockUser.orgID),
   ];
-  const columns = ['job_title', 'organization_id'];
+  const columns = ['job_title', 'organization_id']; // requirements are irrelevant for these tests
+  const returning = ' RETURNING job_id';
   const promises = fakeJobs.map((job) => {
-    const stmt = insert(job, columns, 'job') + ' RETURNING job_id';
+    const stmt = insert(job, columns, 'job') + returning;
     return db.one(stmt);
   });
 
-  Promise.all(promises)
-    .then((res) => {
-      jobIds = res.map((job) => job.job_id);
-    })
-    .finally(done);
+  await Promise.all(promises).then((res) => {
+    jobIds = res.map((job) => job.job_id);
+  });
 });
 
 afterAll(async () => {
@@ -52,24 +59,21 @@ afterAll(async () => {
 
 describe('GET /applicants', () => {
   let applicants: TApplicant[] = [];
-  beforeAll(async (done) => {
-    const promises: any[] = [];
-    // create 20 fake applicants
-    const fakeApplicants = Array(20)
+  beforeAll(async () => {
+    const applicantsCount = faker.random.number({min: 50, max: 100});
+    const fakeApplicants = Array(applicantsCount)
       .fill(0)
       .map(() => {
-        const randomIdx = random.number({min: 0, max: jobIds.length - 1});
-        const randomJobId = jobIds[randomIdx];
-        return fake.applicant(mockUser.orgID, randomJobId);
+        return fake.applicant(mockUser.orgID, randomJobId());
       });
-    fakeApplicants.forEach((applicant) => {
-      const stmt = insert(applicant, null, 'applicant') + ' RETURNING *';
-      promises.push(db.one(stmt));
+
+    const returning = ' RETURNING *';
+    const promises = fakeApplicants.map((applicant) => {
+      const stmt = insert(applicant, null, 'applicant') + returning;
+      return db.one(stmt);
     });
-    Promise.all(promises).then((res) => {
-      applicants = res;
-      done();
-    });
+
+    applicants = await Promise.all(promises);
   });
 
   it('Returns 200 json response', (done) => {
@@ -79,58 +83,63 @@ describe('GET /applicants', () => {
       .expect('Content-Type', /json/)
       .expect(200, done);
   });
+
   it('Returns unfiltered array of applicants', async (done) => {
     const res = await request(app)
       .get('/applicants')
       .set('Accept', 'application/json')
       .expect(200);
-    expect(Array.isArray(res.body)).toBeTruthy();
     expect(res.body.length).toBe(applicants.length);
     done();
   });
+
   it('Filters by job_id using query', async (done) => {
-    const randomIdx = random.number({min: 0, max: jobIds.length - 1});
-    const randomJobId = jobIds[randomIdx];
-    const filteredAppl = applicants.filter(
-      (appl) => appl.job_id === randomJobId,
-    );
+    const jobId = randomJobId();
     const res = await request(app)
-      .get('/applicants?job_id=' + randomJobId)
+      .get('/applicants?job_id=' + jobId)
       .set('Accept', 'application/json')
       .expect(200);
+
+    // filter manually to check request results
+    const filteredAppl = applicants.filter((appl) => appl.job_id === jobId);
     expect(res.body.length).toBe(filteredAppl.length);
+
     done();
   });
+
   it('Includes boolean weather screening exists or not', async (done) => {
-    // insert screening form
-    const randomIdx = random.number({min: 0, max: jobIds.length - 1});
-    const randomJobId = jobIds[randomIdx];
     const {form_id} = await insertForm(
-      fake.screeningForm(mockUser.orgID, randomJobId),
+      fake.screeningForm(mockUser.orgID, randomJobId()),
     );
+
     // insert screening for single applicant
     const randomApplIdx = random.number({min: 0, max: applicants.length - 1});
     const randomApplId = applicants[randomApplIdx].applicant_id;
+
     const range = {min: 0, max: 5};
     const screening = {
       form_id,
       applicant_id: randomApplId,
       submitter_id: mockUser.sub,
+      comment: random.words(),
       submission: {
-        [random.alphaNumeric()]: random.number(range),
-        [random.alphaNumeric()]: random.number(range),
-        [random.alphaNumeric()]: random.number(range),
-        [random.alphaNumeric()]: random.number(range),
+        [random.uuid()]: random.number(range),
+        [random.uuid()]: random.number(range),
+        [random.uuid()]: random.number(range),
+        [random.uuid()]: random.number(range),
       },
     };
     await insertScreening(screening);
+
     const res = await request(app)
       .get('/applicants')
       .set('Accept', 'application/json')
       .expect(200);
+
     const filtered = res.body.filter((appl: any) => appl.screening_exists);
     expect(filtered.length).toBe(1);
     expect(filtered[0].applicant_id).toBe(randomApplId);
+
     done();
   });
 });
