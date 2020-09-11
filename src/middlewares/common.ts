719be2a36d2ffe7dfc26728df724fbcd15341fec
Johannes Oster <johannes.oster@outlook.at>
2020-09-11 11:31:13 +0200

    Update error middleware to use BaseError

--- src/middlewares/common.ts
+++ src/middlewares/common.ts
@@ -1,15 +1,15 @@
 import {RequestHandler, ErrorRequestHandler} from 'express';
 import {validationResult} from 'express-validator';
+import {BaseError} from 'errorHandling';
 
-export const notFound: RequestHandler = (req, res, next) => {
-  res.status(404);
-  const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
-  next(error);
+export const notFound: RequestHandler = (req, res) => {
+  throw new BaseError(404, `🔍 - Not Found - ${req.originalUrl}`);
 };
 
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   console.log(err);
-  const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+  let statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+  if (err.statusCode) statusCode = err.statusCode;
   res.status(statusCode).json({message: err.message, stack: err.stack});
 };
 
