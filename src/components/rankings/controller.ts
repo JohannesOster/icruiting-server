02ccf4404b7fa01a89e2686b0126c074f8eb49a6
Johannes Oster <johannes.oster@outlook.at>
2020-09-15 23:51:04 +0200

    Filter out empty strings in aggregation function in rankings

--- src/components/rankings/controller.ts
+++ src/components/rankings/controller.ts
@@ -1,4 +1,4 @@
-import {catchAsync} from 'errorHandling';
+import {BaseError, catchAsync} from 'errorHandling';
 import {dbSelectScreeningRanking, dbSelectAssessmentRanking} from './database';
 import {
   TScreeningRankingRow,
@@ -12,86 +12,51 @@ export const getRanking = catchAsync(async (req, res) => {
   const {orgID: organization_id} = res.locals.user;
 
   const formCategory = req.query.form_category;
+  let data;
   if (formCategory === 'screening') {
-    const data = await dbSelectScreeningRanking(jobId, organization_id);
-    const tmp = data.map((row: TScreeningRankingRow) => {
-      const {submissions} = row;
-
-      const initialValues = (key: EFormItemIntent) => {
-        return {
-          [EFormItemIntent.sumUp]: 0,
-          [EFormItemIntent.aggregate]: [],
-          [EFormItemIntent.countDistinct]: {},
-        }[key];
-      };
-      const submissionsResult = submissions.reduce((acc, curr) => {
-        curr.forEach(({form_field_id, intent, value, label}) => {
-          if (!acc[form_field_id]) {
-            const initialVal = initialValues(intent);
-            acc[form_field_id] = {label, intent, value: initialVal};
-          }
-          switch (intent) {
-            case EFormItemIntent.sumUp:
-              (acc[form_field_id].value as number) += +value;
-              break;
-            case EFormItemIntent.aggregate:
-              acc[form_field_id].value = (acc[form_field_id].value as Array<
-                string
-              >).concat(value.toString());
-              break;
-            case EFormItemIntent.countDistinct:
-              const key = value.toString();
-              const currVal = (acc[form_field_id].value as KeyVal)[key];
-              (acc[form_field_id].value as KeyVal)[key] = (currVal || 0) + 1;
-          }
-        });
-
-        return acc;
-      }, {} as TScreeningResultObject);
-
-      return {result: submissionsResult, ...row};
-    });
-
-    res.status(200).json(tmp);
+    data = await dbSelectScreeningRanking(jobId, organization_id);
   } else if (formCategory === 'assessment') {
-    const data = await dbSelectAssessmentRanking(jobId, organization_id);
-    const tmp = data.map((row: TScreeningRankingRow) => {
-      const {submissions} = row;
+    data = await dbSelectAssessmentRanking(jobId, organization_id);
+  } else throw new BaseError(402, 'Invalid form_category: ' + formCategory);
 
-      const initialValues = (key: EFormItemIntent) => {
-        return {
-          [EFormItemIntent.sumUp]: 0,
-          [EFormItemIntent.aggregate]: [],
-          [EFormItemIntent.countDistinct]: {},
-        }[key];
-      };
-      const submissionsResult = submissions.reduce((acc, curr) => {
-        curr.forEach(({form_field_id, intent, value, label}) => {
-          if (!acc[form_field_id]) {
-            const initialVal = initialValues(intent);
-            acc[form_field_id] = {label, intent, value: initialVal};
-          }
-          switch (intent) {
-            case EFormItemIntent.sumUp:
-              (acc[form_field_id].value as number) += +value;
-              break;
-            case EFormItemIntent.aggregate:
-              acc[form_field_id].value = (acc[form_field_id].value as Array<
-                string
-              >).concat(value.toString());
-              break;
-            case EFormItemIntent.countDistinct:
-              const key = value.toString();
-              const currVal = (acc[form_field_id].value as KeyVal)[key];
-              (acc[form_field_id].value as KeyVal)[key] = (currVal || 0) + 1;
-          }
-        });
+  const tmp = data.map((row: TScreeningRankingRow) => {
+    const {submissions} = row;
 
-        return acc;
-      }, {} as TScreeningResultObject);
+    const initialValues = (key: EFormItemIntent) => {
+      return {
+        [EFormItemIntent.sumUp]: 0,
+        [EFormItemIntent.aggregate]: [],
+        [EFormItemIntent.countDistinct]: {},
+      }[key];
+    };
+    const submissionsResult = submissions.reduce((acc, curr) => {
+      curr.forEach(({form_field_id, intent, value, label}) => {
+        if (!acc[form_field_id]) {
+          const initialVal = initialValues(intent);
+          acc[form_field_id] = {label, intent, value: initialVal};
+        }
+        switch (intent) {
+          case EFormItemIntent.sumUp:
+            (acc[form_field_id].value as number) += +value;
+            break;
+          case EFormItemIntent.aggregate:
+            const val = value.toString();
+            if (!val) break;
+            const currArray = acc[form_field_id].value as Array<string>;
+            acc[form_field_id].value = currArray.concat(val);
+            break;
+          case EFormItemIntent.countDistinct:
+            const key = value.toString();
+            const currVal = (acc[form_field_id].value as KeyVal)[key];
+            (acc[form_field_id].value as KeyVal)[key] = (currVal || 0) + 1;
+        }
+      });
 
-      return {result: submissionsResult, ...row};
-    });
-    res.status(200).json(tmp);
-  }
+      return acc;
+    }, {} as TScreeningResultObject);
+
+    return {result: submissionsResult, ...row};
+  });
+
+  res.status(200).json(tmp);
 });
