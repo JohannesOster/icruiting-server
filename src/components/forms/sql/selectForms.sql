06d44788f1b1ef479e650a5047bbab145bbebc6e
Johannes Oster <johannes.oster@outlook.at>
2020-09-18 13:28:40 +0200

    Order AC-Forms alphabetically

--- src/components/forms/sql/selectForms.sql
+++ src/components/forms/sql/selectForms.sql
@@ -4,4 +4,5 @@ JOIN (SELECT * FROM form_field) as items
 ON items.form_id = form.form_id
 WHERE form.tenant_id = ${tenant_id}
   AND (form.job_id = ${job_id} OR ${job_id} IS NULL)
-GROUP BY form.form_id;
+GROUP BY form.form_id
+ORDER BY form.form_title;
