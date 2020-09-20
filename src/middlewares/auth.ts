15e9473841c550b6d840288f04f6d0559eb7ec5e
Johannes Oster <johannes.oster@outlook.at>
2020-09-20 19:21:09 +0200

    Add catchAsync to auth middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,43 +1,45 @@
 import {RequestHandler} from 'express';
 import CognitoExpress from 'cognito-express';
 import dotenv from 'dotenv';
+import {BaseError, catchAsync} from 'errorHandling';
+
 dotenv.config();
 
-export const requireAuth: RequestHandler = (req, res, next) => {
-  const cognitoExpress = new CognitoExpress({
-    region: process.env.AWS_REGION,
-    cognitoUserPoolId: process.env.AWS_USER_POOL_ID,
-    tokenUse: 'id',
-    tokenExpiration: 3600000,
-  });
+export const requireAuth: RequestHandler = catchAsync(
+  async (req, res, next) => {
+    const cognitoExpress = new CognitoExpress({
+      region: process.env.AWS_REGION,
+      cognitoUserPoolId: process.env.AWS_USER_POOL_ID,
+      tokenUse: 'id',
+      tokenExpiration: 3600000,
+    });
 
-  const token = req.header('Authorization');
-  if (!token)
-    return res.status(401).json({message: 'Baerer Token missing from header'});
+    const authHeader = req.header('Authorization');
+    if (!authHeader) throw new BaseError(401, 'Missing Authorization header');
 
-  if (token.split(' ').length !== 2 || !token.split(' ')[1])
-    return res.status(401).json({message: 'Invalid Token.'});
+    const token = !authHeader.split(' ')[1];
+    if (token) throw new BaseError(401, 'Invalid Token');
 
-  cognitoExpress.validate(token.split(' ')[1], (err: any, payload: any) => {
-    if (err) return res.status(401).json({message: err});
+    cognitoExpress.validate(token, (err: Error, payload: any) => {
+      if (err) throw new BaseError(401, err.message);
 
-    const lastSlashIdx = payload.iss.lastIndexOf('/');
-    const userPoolID = payload.iss.substring(lastSlashIdx + 1);
-    res.locals.user = {
-      tenant_id: payload['custom:tenant_id'],
-      userPoolID: userPoolID,
-      user_id: payload.sub,
-      userRole: payload['custom:user_role'],
-      email: payload.email,
-    };
-    next();
-  });
-};
+      const lastSlashIdx = payload.iss.lastIndexOf('/');
+      const userPoolID = payload.iss.substring(lastSlashIdx + 1);
+      res.locals.user = {
+        tenant_id: payload['custom:tenant_id'],
+        userPoolID: userPoolID,
+        user_id: payload.sub,
+        userRole: payload['custom:user_role'],
+        email: payload.email,
+      };
+
+      next();
+    });
+  },
+);
 
 export const requireAdmin: RequestHandler = (req, res, next) => {
   const userRole = res.locals.user.userRole;
-  if (userRole !== 'admin') {
-    return res.status(401).json({message: 'Admin required.'});
-  }
+  if (userRole !== 'admin') throw new BaseError(401, 'Admin required');
   next();
 };
