160de6214745e4c109b3f59072c92f345eea0b0d
Johannes Oster <johannes.oster@outlook.at>
2020-09-27 23:28:20 +0200

    Replace all.sql with find.sql and conditional WHERE clause

--- src/db/repos/jobs/sql/find.sql
+++ src/db/repos/jobs/sql/find.sql
@@ -3,5 +3,6 @@ FROM job
 JOIN (SELECT * FROM job_requirement) AS jr
 ON jr.job_id = job.job_id
 WHERE job.tenant_id = ${tenant_id}
-  AND job.job_id = ${job_id}
-GROUP BY job.job_id;
+  AND (job.job_id = ${job_id} OR ${job_id} IS NULL)
+GROUP BY job.job_id
+ORDER BY job.created_at DESC;
--- src/db/repos/jobs/sql/index.ts
+++ src/db/repos/jobs/sql/index.ts
@@ -2,6 +2,5 @@ import {sql} from '../../../utils';
 import {join} from 'path';
 
 export default {
-  all: sql(join(__dirname, 'all.sql')),
   find: sql(join(__dirname, 'find.sql')),
 };
