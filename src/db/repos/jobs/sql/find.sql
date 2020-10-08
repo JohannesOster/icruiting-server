d50d7888f88ca1964328758e23cc55883f5e9be8
Johannes Oster <johannes.oster@outlook.at>
2020-10-08 19:03:12 +0200

    Update db jobs.find

--- src/db/repos/jobs/sql/find.sql
+++ src/db/repos/jobs/sql/find.sql
@@ -1,6 +1,6 @@
 SELECT job.*, json_agg(jr) job_requirements
 FROM job
-JOIN (SELECT * FROM job_requirement) AS jr
+JOIN job_requirement jr
 ON jr.job_id = job.job_id
 WHERE job.tenant_id = ${tenant_id}
   AND (job.job_id = ${job_id} OR ${job_id} IS NULL)
