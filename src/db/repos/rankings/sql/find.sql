d30559b655f1bf41b45981a92044ee6016f8c661
Johannes Oster <johannes.oster@outlook.at>
2020-10-08 18:58:59 +0200

    Update db rankings.find

--- src/db/repos/rankings/sql/find.sql
+++ src/db/repos/rankings/sql/find.sql
@@ -1,14 +1,13 @@
 SELECT
 	applicant_id,
 	COUNT(DISTINCT submitter_id) AS submissions_count,
-	ROUND(STDDEV_POP(single_submission.score), 2) AS standard_deviation,
-	ROUND(AVG(single_submission.score), 2) AS score,
-  ROW_NUMBER() OVER (ORDER BY AVG(single_submission.score) DESC) AS rank,
-	ARRAY_AGG(single_submission.submission) AS submissions
-FROM
-	(SELECT * FROM form_submission_view
-	 WHERE form_category=${form_category}
-	   AND tenant_id=${tenant_id}
-		 AND job_id=${job_id}) AS single_submission
+	ROUND(STDDEV_POP(score), 2) AS standard_deviation,
+	ROUND(AVG(score), 2) AS score,
+  ROW_NUMBER() OVER (ORDER BY AVG(score) DESC) AS rank,
+	ARRAY_AGG(submission) AS submissions
+FROM form_submission_view
+WHERE form_category=${form_category}
+	AND tenant_id=${tenant_id}
+	AND job_id=${job_id}
 GROUP BY applicant_id
 ORDER BY rank;
