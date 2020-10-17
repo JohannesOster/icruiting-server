9787747d7f37a3c4c55bba3d91aaedb5de089d09
Johannes Oster <johannes.oster@outlook.at>
2020-10-17 12:02:32 +0200

    Refactor applicants all query

--- src/db/repos/applicants/sql/all.sql
+++ src/db/repos/applicants/sql/all.sql
@@ -3,11 +3,9 @@ SELECT applicant.*,
        screening_exists(${tenant_id}, ${user_id}, applicant.applicant_id)
 FROM applicant_view AS applicant
 LEFT JOIN (
-	SELECT attribute_value AS order_value, applicant_id
-	FROM applicant_attribute
-	LEFT JOIN form_field
-	ON applicant_attribute.form_field_id = form_field.form_field_id
-	WHERE form_field.label = ${order_by}) AS order_query
+	SELECT applicant_id, attribute->>'value' AS order_value
+	FROM (SELECT applicant_id, unnest(attributes) as attribute FROM applicant_view) AS attribute
+	WHERE attribute->>'key' = ${order_by}) AS order_query
 ON ${order_by} IS NOT NULL AND order_query.applicant_id = applicant.applicant_id
 WHERE applicant.tenant_id = ${tenant_id}
   AND (applicant.job_id = ${job_id} OR ${job_id} IS NULL)
