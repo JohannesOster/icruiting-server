5820c48bb27df247d8151f7539087a85747a76b3
Johannes Oster <johannes.oster@outlook.at>
2020-11-05 21:38:19 +0100

    Hide error stack in production

--- src/middlewares/common.ts
+++ src/middlewares/common.ts
@@ -10,7 +10,10 @@ export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   // console.log(err);
   let statusCode = res.statusCode !== 200 ? res.statusCode : 500;
   if (err.statusCode) statusCode = err.statusCode;
-  res.status(statusCode).json({message: err.message, stack: err.stack});
+  res.status(statusCode).json({
+    message: err.message,
+    ...(process.env.NODE_ENV !== 'production' ? {stack: err.stack} : {}),
+  });
 };
 
 export const validate: RequestHandler = (req, res, next) => {
