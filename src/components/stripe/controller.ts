77f0fcadc736047e8d9bb77edf66d66d245e3b63
Johannes Oster <johannes.oster@outlook.at>
2020-11-05 11:15:15 +0100

    Add boilerplate stripe webhook endpoint

--- src/components/stripe/controller.ts
+++ src/components/stripe/controller.ts
@@ -9,3 +9,24 @@ export const getPrices = catchAsync(async (req, res) => {
   const {data} = await stripe.prices.list({limit: 3, expand: ['data.product']});
   res.status(201).json(data);
 });
+
+export const webhook = catchAsync(async (req, res) => {
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      (req as any).rawBody,
+      req.header('Stripe-Signature') || '',
+      process.env.STRIPE_WEBHOOK_SECRET || '',
+    );
+  } catch (err) {
+    console.log(err);
+    console.log(`⚠️  Webhook signature verification failed.`);
+    console.log(`⚠️  Check the env file and enter the correct webhook secret.`);
+    return res.sendStatus(400);
+  }
+
+  console.log(event);
+
+  res.json({received: true});
+});
--- src/components/stripe/routes.ts
+++ src/components/stripe/routes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import {getPrices} from './controller';
+import {getPrices, webhook} from './controller';
 
 const router = express.Router();
 
 router.get('/prices', getPrices);
+router.post('/webhook', webhook);
 
 export {router as routes};
