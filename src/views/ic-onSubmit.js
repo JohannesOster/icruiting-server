38124201c05d82c809784ab083c115a9d36cac9a
Johannes Oster <johannes.oster@outlook.at>
2020-11-09 16:15:19 +0100

    Update form pug template and css

--- src/views/ic-onSubmit.js
+++ src/views/ic-onSubmit.js
@@ -1,5 +1,5 @@
 function onSubmit() {
   const buttonId = 'ic-submit-button';
   const button = document.getElementById(buttonId);
-  button.classList.add('ic-loading');
+  button.classList.add('loading');
 }
