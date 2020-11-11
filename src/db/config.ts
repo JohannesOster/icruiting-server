2276378231746dc7a97f6157aee2fb3148afb17e
Johannes Oster <johannes.oster@outlook.at>
2020-11-11 21:18:07 +0100

    Add prod config for flyway and include db-migrate on startup

--- src/db/config.ts
+++ src/db/config.ts
@@ -5,7 +5,15 @@ dotenv.config();
 type Environment = 'production' | 'development' | 'test';
 
 const config = {
-  production: {url: process.env.DATABASE_URL || '', flyway: {}},
+  production: {
+    url: process.env.DATABASE_URL || '',
+    flyway: {
+      url: `jdbc:${process.env.FLYWAY_PROD_DB_URL}`,
+      user: process.env.FLYWAY_PROD_DB_USER,
+      password: process.env.FLYWAY_PROD_DB_PASSWORD,
+      locations: 'filesystem:src/db/migrations',
+    },
+  },
   development: {
     url: process.env.DEV_DB_URL || '',
     flyway: {
