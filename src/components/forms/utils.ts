d0dca14a9278a556e2cb71732d3c67464f1ce0e3
Johannes Oster <johannes.oster@outlook.at>
2020-11-12 22:25:54 +0100

    Fix stripe subscription validation for html form endpoints

--- src/components/forms/utils.ts
+++ src/components/forms/utils.ts
@@ -1,5 +1,10 @@
+import Stripe from 'stripe';
 import db from 'db';
 import {BaseError} from 'errorHandling';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
+  apiVersion: '2020-08-27',
+});
 
 export const validateSubscription = async (tenantId: string) => {
   const tenant = await db.oneOrNone(
@@ -8,15 +13,21 @@ export const validateSubscription = async (tenantId: string) => {
   );
   if (!tenant) throw new BaseError(404, 'Tenant Not Found');
 
-  const {stripeSubscriptionId, stripeSubscriptionStatus = ''} = tenant;
-  if (!stripeSubscriptionId) {
-    throw new BaseError(401, 'Subscription required');
-  }
+  if (!tenant.stripeCustomerId)
+    throw new BaseError(404, 'Stripe customer id not found');
 
-  if (!['active', 'trialing'].includes(stripeSubscriptionStatus)) {
-    throw new BaseError(
-      401,
-      `Invalid subscription status ${stripeSubscriptionStatus}`,
-    );
-  }
+  const subsCount = await Promise.all([
+    stripe.subscriptions.list({
+      customer: tenant.stripeCustomerId,
+      status: 'active',
+    }),
+    stripe.subscriptions.list({
+      customer: tenant.stripeCustomerId,
+      status: 'trialing',
+    }),
+  ]).then(([active, trailing]) => {
+    return active.data.length + trailing.data.length;
+  });
+
+  if (subsCount < 1) throw new BaseError(401, 'Subscription required');
 };
