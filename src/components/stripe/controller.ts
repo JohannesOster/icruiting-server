5259b200068bcc20584ae6bed92700b823c43a1f
Johannes Oster <johannes.oster@outlook.at>
2020-11-13 14:47:40 +0100

    Filter inActive prices and friends and family plan

--- src/components/stripe/controller.ts
+++ src/components/stripe/controller.ts
@@ -1,6 +1,7 @@
 import Stripe from 'stripe';
 import {catchAsync} from 'errorHandling';
 import webhookHandler from './webhookHandler';
+import config from 'config';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
   apiVersion: '2020-08-27',
@@ -8,7 +9,14 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
 
 export const getPrices = catchAsync(async (req, res) => {
   const {data} = await stripe.prices.list({limit: 3, expand: ['data.product']});
-  res.status(201).json(data);
+  const filterd = data.filter((price) => {
+    const isFriendsAndFamiliy =
+      (price.product as any).id === config.freeStripeProducId;
+    const isActive = price.active;
+    return isActive && !isFriendsAndFamiliy;
+  });
+
+  res.status(201).json(filterd);
 });
 
 export const webhook = catchAsync(async (req, res) => {
