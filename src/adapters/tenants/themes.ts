779804756bcf2ff6160756641a1265b6d5e6e2bf
Johannes Oster <johannes.oster@outlook.at>
2021-02-13 20:15:46 +0100

    Remove domain folder

--- src/adapters/tenants/themes.ts
+++ src/adapters/tenants/themes.ts
@@ -1,9 +1,10 @@
 import {IncomingForm} from 'formidable';
 import {BaseError, httpReqHandler} from 'adapters/errorHandling';
-import {ThemesService} from 'domain/services/tenants/themes';
+import fs from 'fs';
+import {S3} from 'aws-sdk';
+import db from 'infrastructure/db';
 
 export const ThemesAdapter = () => {
-  const themes = ThemesService();
   const upload = httpReqHandler(async (req) => {
     const {tenantId} = req.user;
     const formidable = new IncomingForm();
@@ -17,7 +18,25 @@ export const ThemesAdapter = () => {
         if (extension !== 'css')
           return reject(new BaseError(422, `Invalid fileformat ${extension}`));
 
-        await themes.upload(tenantId, file);
+        const tenant = await db.tenants.retrieve(tenantId);
+        if (!tenant) throw new BaseError(404, 'Tenant Not Found');
+
+        const s3 = new S3();
+        const bucket = process.env.S3_BUCKET!;
+
+        const fileId = (Math.random() * 1e32).toString(36);
+        const fileKey = tenant.theme || tenantId + '.' + fileId + '.css';
+        const fileStream = fs.createReadStream(file.path);
+
+        const params = {
+          Key: fileKey,
+          Bucket: bucket,
+          ContentType: file.type,
+          Body: fileStream,
+        };
+        await s3.upload(params).promise();
+
+        if (!tenant.theme) await db.tenants.updateTheme(tenantId, fileKey);
 
         resolve({
           status: 201,
@@ -29,7 +48,18 @@ export const ThemesAdapter = () => {
 
   const del = httpReqHandler(async (req) => {
     const {tenantId} = req.user;
-    await themes.del(tenantId);
+    const tenant = await db.tenants.retrieve(tenantId);
+    if (!tenant) throw new BaseError(404, 'Tenant Not Found');
+    if (!tenant.theme) throw new BaseError(404, 'Theme Not Found');
+
+    const s3 = new S3();
+    const bucket = process.env.S3_BUCKET!;
+    const delParams = {
+      Bucket: bucket,
+      Delete: {Objects: [{Key: tenant.theme!}]},
+    };
+    await s3.deleteObjects(delParams).promise();
+    await db.tenants.updateTheme(tenantId, null);
     return {};
   });
 
