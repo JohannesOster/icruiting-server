a0c2d6f3febd3ef9bca834884cbfc308c31e8831
Johannes Oster <johannes.oster@outlook.at>
2021-02-15 00:13:28 +0100

    Refactor sumbmitHTMLform

--- src/adapters/forms/controller.ts
+++ src/adapters/forms/controller.ts
@@ -77,7 +77,7 @@ export const FormsAdapter = () => {
     }
 
     const submitAction = config.baseURL + req.originalUrl;
-    const params: any = {formId, submitAction, formFields: form.formFields};
+    const params = {formId, submitAction, formFields: form.formFields};
     const tenant = await db.tenants.retrieve(form.tenantId);
     if (!tenant?.theme) return {view: 'form', body: params};
 
@@ -91,11 +91,13 @@ export const FormsAdapter = () => {
     const {formId} = req.params;
     const form = await db.forms.retrieve(null, formId);
     if (!form) throw new BaseError(404, 'Not Found');
+    const tenant = await db.tenants.retrieve(form.tenantId);
+    if (!tenant) throw new BaseError(404, 'Tenant Not Found');
 
     try {
       await validateSubscription(form.tenantId);
-    } catch (error) {
-      return {view: 'form-submission', body: {error}};
+    } catch ({message}) {
+      return {view: 'form-submission', body: {error: message}};
     }
 
     if (form.formCategory !== 'application') {
@@ -104,97 +106,92 @@ export const FormsAdapter = () => {
       throw new BaseError(402, errorMsg);
     }
 
-    const applicant: any = {tenantId: form.tenantId, jobId: form.jobId};
-
     const formidable = new IncomingForm();
     formidable.maxFileSize = 500 * 1024 * 1024;
     return new Promise((resolve) => {
       formidable.parse(req, async (error, fields, files) => {
-        if (error) resolve({view: 'form-submission', body: {error}});
+        if (error) return resolve({view: 'form-submission', body: {error}});
 
         const s3 = new S3();
         const promises = [];
 
-        /* currently there is no other way to get out of a reduce loop */
-        let map = {attributes: []} as {
-          attributes: {formFieldId: string; attributeValue: string}[];
-        };
+        type Attributes = {formFieldId: string; attributeValue: string}[];
+        let attributes: Attributes;
         try {
-          map = form.formFields.reduce(
-            (acc, item) => {
-              // !> filter out non submitted values
-              const fieldExists = fields[item.formFieldId];
-              let file = files[item.formFieldId];
-              if (Array.isArray(file)) file = file[0];
-              const fileExists = file && file.size;
-              if (!fieldExists && !fileExists) {
-                if (item.required) {
-                  throw new BaseError(
-                    402,
-                    `Missing required field: ${item.label}`,
-                  );
-                }
-
-                return acc;
-              }
-
-              if (
-                ['input', 'textarea', 'select', 'radio'].includes(
-                  item.component,
-                )
-              ) {
-                acc.attributes.push({
-                  formFieldId: item.formFieldId,
-                  attributeValue: fields[item.formFieldId] as string,
-                });
-              } else if (item.component === 'checkbox') {
-                const value = Array.isArray(fields[item.formFieldId])
-                  ? (fields[item.formFieldId] as string[]).join(', ')
-                  : (fields[item.formFieldId] as string);
-
-                acc.attributes.push({
-                  formFieldId: item.formFieldId,
-                  attributeValue: value,
-                });
-              } else if (item.component === 'file_upload') {
-                const extension = file.name.substr(
-                  file.name.lastIndexOf('.') + 1,
+          attributes = form.formFields.reduce((acc, item) => {
+            // !> filter out non submitted values
+            const fieldExists = fields[item.formFieldId];
+            let file = files[item.formFieldId];
+            if (Array.isArray(file)) file = file[0];
+            const fileExists = file && file.size;
+            if (!fieldExists && !fileExists) {
+              if (item.required) {
+                throw new BaseError(
+                  402,
+                  `Missing required field: ${item.label}`,
                 );
-                const fileId = (Math.random() * 1e32).toString(36);
-                const fileKey = form.tenantId + '.' + fileId + '.' + extension;
-                const fileStream = fs.createReadStream(file.path);
-                const params = {
-                  Key: fileKey,
-                  Bucket: process.env.S3_BUCKET || '',
-                  ContentType: file.type,
-                  Body: fileStream,
-                };
-
-                fs.unlink(file.path, console.error);
-
-                promises.push(s3.upload(params).promise());
-                acc.attributes.push({
-                  formFieldId: item.formFieldId,
-                  attributeValue: fileKey,
-                });
               }
 
               return acc;
-            },
-            {attributes: []} as {
-              attributes: {formFieldId: string; attributeValue: string}[];
-            },
-          );
+            }
+
+            if (
+              ['input', 'textarea', 'select', 'radio'].includes(item.component)
+            ) {
+              acc.push({
+                formFieldId: item.formFieldId,
+                attributeValue: fields[item.formFieldId] as string,
+              });
+            } else if (item.component === 'checkbox') {
+              const value = Array.isArray(fields[item.formFieldId])
+                ? (fields[item.formFieldId] as string[]).join(', ')
+                : (fields[item.formFieldId] as string);
+
+              acc.push({
+                formFieldId: item.formFieldId,
+                attributeValue: value,
+              });
+            } else if (item.component === 'file_upload') {
+              const extension = file.name.substr(
+                file.name.lastIndexOf('.') + 1,
+              );
+              const fileId = (Math.random() * 1e32).toString(36);
+              const fileKey = form.tenantId + '.' + fileId + '.' + extension;
+              const fileStream = fs.createReadStream(file.path);
+              const params = {
+                Key: fileKey,
+                Bucket: process.env.S3_BUCKET || '',
+                ContentType: file.type,
+                Body: fileStream,
+              };
+
+              fs.unlink(file.path, console.error);
+
+              promises.push(s3.upload(params).promise());
+              acc.push({
+                formFieldId: item.formFieldId,
+                attributeValue: fileKey,
+              });
+            }
+
+            return acc;
+          }, [] as Attributes);
         } catch (error) {
           return resolve({view: 'form-submission', body: {error}});
         }
 
-        applicant.attributes = map.attributes;
+        const applicant = {
+          tenantId: form.tenantId,
+          jobId: form.jobId,
+          attributes,
+        };
         promises.push(db.applicants.create(applicant));
 
-        const resp = await Promise.all(promises)
-          .then(() => ({view: 'form-submission'}))
-          .catch((error) => ({view: 'form-submission', body: {error}}));
+        try {
+          await Promise.all(promises);
+        } catch ({message}) {
+          return resolve({view: 'form-submission', body: {error: message}});
+        }
 
         const {emailFieldId, fullNameFieldId} = form.formFields.reduce(
           (acc, curr) => {
@@ -223,18 +220,12 @@ export const FormsAdapter = () => {
         if (!fullName)
           throw new BaseError(500, 'Applicant has no email-adress');
 
-        const mailOptions = {
-          to: email,
-          subject: 'Bewerbungsbestätigung',
-          html: templates(Template.EmailConfirmation, {
-            tenantName: (await db.tenants.retrieve(form.tenantId))?.tenantName,
-            fullName,
-          }),
-        };
-
+        const templateOptions = {tenantName: tenant.tenantName, fullName};
+        const html = templates(Template.EmailConfirmation, templateOptions);
+        const mailOptions = {to: email, subject: 'Bewerbungsbestätigung', html};
         await sendMail(mailOptions).catch(console.error);
 
-        resolve(resp);
+        resolve({view: 'form-submission'});
       });
     });
   });
