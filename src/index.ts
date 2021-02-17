14bf9128188e4eaa83042f08aca32e8cf233d326
Johannes Oster <johannes.oster@outlook.at>
2021-02-17 20:19:53 +0100

    Fix port assignment error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import app from 'infrastructure/http';
 
-app.listen(process.env.PORT || 5000, () => {
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
   if (process.env.NODE_ENV === 'development') {
     /* tslint:disable no-console */
     console.log(`Listening: http://localhost:${port}`);
