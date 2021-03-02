c2992737b22d3443faec805590cdcfe458f8260b
Johannes Oster <johannes.oster@outlook.at>
2021-03-02 20:40:43 +0100

    Add confirmed column to applicant

--- src/domain/entities/applicant.ts
+++ src/domain/entities/applicant.ts
@@ -3,6 +3,7 @@ import {v4 as uuidv4} from 'uuid';
 export type Attribute = {formFieldId: string; value: string};
 export type File = {formFieldId: string; uri: string};
 type BaseApplicant = {
+  confirmed: boolean;
   tenantId: string;
   jobId: string;
   attributes: Attribute[];
