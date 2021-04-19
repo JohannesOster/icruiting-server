a85adb21edf1b604dacccd1c9bdc181ae5f1ee1e
Johannes Oster <johannes.oster@outlook.at>
2021-04-19 14:13:39 +0200

    Fix logging statements

--- src/application/applicants/calcReport/calcReport.ts
+++ src/application/applicants/calcReport/calcReport.ts
@@ -5,6 +5,7 @@ import {ReportBuilder} from './reportBuilder';
 import {JobRequirement} from 'domain/entities';
 import {mergeReplicas} from './mergeReplicas';
 import {createReport} from './report';
+import {inspect} from 'util';
 
 export const calcReport = (
   rows: ReportPrepareRow[],
@@ -13,11 +14,11 @@ export const calcReport = (
 ) => {
   const [forms, formFields] = filterFormData(rows);
   const submissions = reduceSubmissions(rows);
-  console.log(submissions.applicant1.submitter1);
+  console.log(inspect(submissions, {depth: null}));
   const raw = ReportBuilder(formFields, submissions);
-  console.log(raw.formFieldScores.applicant1);
+  console.log(inspect(raw, {depth: null}));
   const report = mergeReplicas(raw, forms);
-  console.log(report.formFieldScores.applicant1.form1.replicas);
+  console.log(inspect(report, {depth: null}));
 
   const _forms = Object.entries(forms).reduce((acc, [formId, form]) => {
     acc[formId] = {...form, formFields: formFields[formId]};
