473f92b74507e2d2e47f386bd24f400e75a6268e
Johannes Oster <johannes.oster@outlook.at>
2021-04-19 14:06:20 +0200

    Add logging stmts in calcReport

--- src/application/applicants/calcReport/calcReport.ts
+++ src/application/applicants/calcReport/calcReport.ts
@@ -13,8 +13,11 @@ export const calcReport = (
 ) => {
   const [forms, formFields] = filterFormData(rows);
   const submissions = reduceSubmissions(rows);
+  console.log(submissions.applicant1.submitter1);
   const raw = ReportBuilder(formFields, submissions);
+  console.log(raw.formFieldScores.applicant1);
   const report = mergeReplicas(raw, forms);
+  console.log(report.formFieldScores.applicant1.form1.replicas);
 
   const _forms = Object.entries(forms).reduce((acc, [formId, form]) => {
     acc[formId] = {...form, formFields: formFields[formId]};
@@ -28,6 +31,8 @@ export const calcReport = (
     },
     {} as any,
   );
+
+  console.log(createReport(applicantId, report, _forms, _jobRequirements));
 
   return createReport(applicantId, report, _forms, _jobRequirements);
 };
