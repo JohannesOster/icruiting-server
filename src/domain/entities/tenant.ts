f71169afa4a80377d3e0fcd534922508356da36f
Johannes Oster <johannes.oster@outlook.at>
2021-08-06 22:48:20 +0200

    Add creatTenant factory unit tests

--- src/domain/entities/tenant.ts
+++ src/domain/entities/tenant.ts
@@ -1,18 +1,26 @@
-import {v4 as uuidv4} from 'uuid';
+import {v4 as uuid} from 'uuid';
 
 type BaseTenant = {
+  /** A short name of the overall group of users, the organization, etc. */
   tenantName: string;
+  /** A stripe customer id */
   stripeCustomerId?: string;
+  /** A url to a theme file */
   theme?: string;
 };
 
-export type Tenant = {tenantId: string} & BaseTenant;
+export type Tenant = {
+  /** A unique id */
+  tenantId: string;
+} & BaseTenant;
 
 export const createTenant = (
   tenant: BaseTenant & {tenantId?: string},
 ): Tenant => {
-  return Object.freeze({
-    ...tenant,
-    tenantId: tenant.tenantId || uuidv4(),
-  });
+  const tenantId = tenant.tenantId || uuid();
+
+  if (!tenant.tenantName.length)
+    throw new Error('Property tenantName cannot be empty string');
+
+  return Object.freeze({...tenant, tenantId});
 };
