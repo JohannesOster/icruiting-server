b751eeb947b7ff370f0a218b7f89f3abf822a31c
Johannes Oster <johannes.oster@outlook.at>
2021-08-06 23:04:33 +0200

    Add createJob factory unit tests

--- src/domain/entities/job.ts
+++ src/domain/entities/job.ts
@@ -1,13 +1,30 @@
-import {v4 as uuidv4} from 'uuid';
+import {v4 as uuid} from 'uuid';
 
-type BaseJobRequirement = {requirementLabel: string; minValue?: number};
+type BaseJobRequirement = {
+  /** A human readable term for the requirement. */
+  requirementLabel: string;
+  /** The minimal value for this requirement (relevant for assessment evaluations) */
+  minValue?: number;
+};
 export type JobRequirement = {
+  /** A unique id */
   jobRequirementId: string;
+  /** The unique id of the job the requirement belongs to */
   jobId: string;
 } & BaseJobRequirement;
 
-type BaseJob = {tenantId: string; jobTitle: string};
-export type Job = {jobId: string; jobRequirements: JobRequirement[]} & BaseJob;
+type BaseJob = {
+  /** The unique id of the tenant the job belongs to */
+  tenantId: string;
+  /** A human readable term for the job */
+  jobTitle: string;
+};
+export type Job = {
+  /** A unique id */
+  jobId: string;
+  /** A list of requirements for the job */
+  jobRequirements: JobRequirement[];
+} & BaseJob;
 
 export const createJob = (
   job: BaseJob & {
@@ -15,14 +32,14 @@ export const createJob = (
     jobRequirements: (BaseJobRequirement & {jobRequirementId?: string})[];
   },
 ): Job => {
-  const jobId = job.jobId || uuidv4();
-  return Object.freeze({
-    ...job,
+  const jobId = job.jobId || uuid();
+
+  // Add appropriate jobId and assign uuid if it does not exist
+  const jobRequirements = job.jobRequirements.map((requirement) => ({
+    ...requirement,
     jobId,
-    jobRequirements: job.jobRequirements.map((requirement) => ({
-      ...requirement,
-      jobId,
-      jobRequirementId: requirement.jobRequirementId || uuidv4(),
-    })),
-  });
+    jobRequirementId: requirement.jobRequirementId || uuid(),
+  }));
+
+  return Object.freeze({...job, jobId, jobRequirements});
 };
