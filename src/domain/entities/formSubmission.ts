1d1f66b037e91063970f9a05ffbfa124277c815e
Johannes Oster <johannes.oster@outlook.at>
2021-08-06 23:10:35 +0200

    Fix typo and add docstrings

--- src/domain/entities/formSubmission.ts
+++ src/domain/entities/formSubmission.ts
@@ -1,10 +1,18 @@
 import {v4 as uuid} from 'uuid';
 
 type BaseFormSubmission = {
+  /** The unique id of the tenant the formSubission belongs to */
   tenantId: string;
+  /** The unique id of the form the formSubmission belongs to */
   formId: string;
+  /** The unique id of the user the formSubmission was created by */
   submitterId: string;
+  /** The unique id of the applicant the formSubmission refers to */
   applicantId: string;
+  /** The submission values as key-value pairs
+   * key: The unique id of the formField the value was submitted with
+   * value: The submission value
+   */
   submission: {[formFieldId: string]: string};
 };
 
@@ -13,8 +21,6 @@ export type FormSubmission = {formSubmissionId: string} & BaseFormSubmission;
 export const createFormSubmission = (
   formSubmission: BaseFormSubmission & {formSubmissionId?: string},
 ): FormSubmission => {
-  return Object.freeze({
-    ...formSubmission,
-    formSubmissionId: formSubmission.formSubmissionId || uuid(),
-  });
+  const formSubmissionId = formSubmission.formSubmissionId || uuid();
+  return Object.freeze({...formSubmission, formSubmissionId});
 };
