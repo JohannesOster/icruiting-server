29897bce9dfd8d77c896eac9c2d61ebcfcc7e0d4
Johannes Oster <johannes.oster@outlook.at>
2021-09-17 08:34:22 +0200

    Create separate members module

---
+++ src/modules/members/infrastructure/http/router.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import {retrieveRules, updateRules} from './validation';
+import {validate} from 'infrastructure/http/middlewares/common';
+import {requireAdmin, requireAuth} from 'infrastructure/http/middlewares';
+import {requireSubscription} from 'infrastructure/http/middlewares/stripe';
+import {RouterFactory} from 'infrastructure/http';
+import {MembersAdapter} from 'modules/members/application';
+
+export const MembersRouter: RouterFactory = () => {
+  const adapter = MembersAdapter();
+  const router = express.Router();
+
+  router.use(requireAuth);
+  router.use(requireSubscription);
+  router.use(requireAdmin);
+  router.post('/', retrieveRules, validate, adapter.create);
+  router.get('/', adapter.list);
+  router.put('/:username', updateRules, validate, adapter.update);
+  router.delete('/:username', adapter.del);
+
+  return router;
+};
