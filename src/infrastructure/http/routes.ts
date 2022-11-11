8b2ab13640a49ea949f70011b99db9731103a896
Johannes Oster <johannes.oster@outlook.at>
2022-11-12 00:47:15 +0100

    Add /health route to monitor api status

--- src/infrastructure/http/routes.ts
+++ src/infrastructure/http/routes.ts
@@ -20,9 +20,14 @@ router.use('/form-submissions', FormSubmissionRouter({db, pgp}));
 router.use('/applicants', ApplicantsRouter({db, pgp}));
 router.use('/rankings', RankingsRouter({db, pgp}));
 
-// This route exists for easier development of email .pug templates
-router.get('/mail', (req, res, next) => {
-  res.render('application-confirmation-email');
+// route to monitor server status by elastic beanstalk
+router.get('/health', (req, res) => {
+  res.json('Everything Up And Running! 🎉');
 });
+
+// // This route exists for easier development of email .pug templates
+// router.get('/mail', (req, res, next) => {
+//   res.render('application-confirmation-email');
+// });
 
 export {router as routes};
