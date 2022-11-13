35ca5dd27cb1535d29a432796ce39f6fbed274aa
Johannes Oster <johannes.oster@outlook.at>
2022-11-13 15:34:22 +0100

    Remove failing test

--- tests/unit/utils.test.ts
+++ tests/unit/utils.test.ts
@@ -7,7 +7,6 @@ import {removePrefix} from '../../src/infrastructure/authService/utils';
 describe('utils', () => {
   describe('removePrefix', () => {
     it('removes prefix', () => {
-      expect(1).toBe(2); // failing test to see whether build stops
       const prefix = 'custom:';
       const attributeName = faker.random.word();
 
