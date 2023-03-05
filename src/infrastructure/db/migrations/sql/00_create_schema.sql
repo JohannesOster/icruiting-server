4db0bdd4ae72495bdcbe9648f0f2704fcaa680bd
Johannes Oster <johannes.oster@outlook.at>
2023-03-05 12:37:39 +0100

    Remove flyway from initial migration

--- src/infrastructure/db/migrations/sql/00_create_schema.sql
+++ src/infrastructure/db/migrations/sql/00_create_schema.sql
@@ -2,22 +2,6 @@
 -- changeset johannesoster:1
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE flyway_schema_history (
-  installed_rank INTEGER NOT NULL,
-  version VARCHAR(50),
-  description VARCHAR(200) NOT NULL,
-  type VARCHAR(20) NOT NULL,
-  script VARCHAR(1000) NOT NULL,
-  checksum INTEGER,
-  installed_by VARCHAR(100) NOT NULL,
-  installed_on TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
-  execution_time INTEGER NOT NULL,
-  success BOOLEAN NOT NULL,
-  CONSTRAINT flyway_schema_history_pk PRIMARY KEY (installed_rank)
-);
-
-CREATE INDEX flyway_schema_history_s_idx ON flyway_schema_history(success);
-
 CREATE TABLE tenant (
   tenant_id UUID DEFAULT uuid_generate_v4() NOT NULL,
   tenant_name TEXT NOT NULL,
