31b11d1115287deb281f233ea1f53de8a556f06a
Johannes Oster <johannes.oster@outlook.at>
2023-04-05 12:17:40 +0200

    Make requirements optional in update job

--- src/modules/jobs/infrastructure/http/validation.ts
+++ src/modules/jobs/infrastructure/http/validation.ts
@@ -3,7 +3,7 @@ import {body} from 'express-validator';
 export const createRules = [body('jobTitle').isString()];
 export const updateRules = [
   body('jobTitle').isString(),
-  body('jobRequirements').isArray(),
+  body('jobRequirements').isArray().optional({nullable: true}),
   body('jobRequirements[*].requirementLabel').isString(),
   body('jobRequirements[*].minValue').optional({nullable: true}).isNumeric().toFloat(),
 ];
