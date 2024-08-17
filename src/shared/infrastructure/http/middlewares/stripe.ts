5339ffb5dbbebae1f0f37f2272c2e59ffabf656e
Johannes Oster <johannes.oster@outlook.at>
2024-08-17 21:48:30 +0200

    Replace db "one" query with saver "oneOrNone"
    "one" throws non-differntiable error, in order to properly handle case of non-existing tenant in requireStripeSubscription we replaced it with safer "oneOrNone".

--- src/shared/infrastructure/http/middlewares/stripe.ts
+++ src/shared/infrastructure/http/middlewares/stripe.ts
@@ -9,7 +9,7 @@ export const requireSubscription = catchAsync(async (req, res, next) => {
   if (!tenantId) tenantId = req.params.tenantId;
   if (!tenantId) throw new BaseError(422, 'Missing tenantId');
 
-  const tenant = await db.one('SELECT * FROM tenant where tenant_id=$1', tenantId); // TODO: find better solution
+  const tenant = await db.oneOrNone('SELECT * FROM tenant where tenant_id=$1', tenantId); // TODO: find better solution
   if (!tenant) throw new BaseError(404, 'Authorized tenant Not Found.');
 
   if (!tenant.stripeCustomerId) throw new BaseError(404, 'Stripe customer id not found');
