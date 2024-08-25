fe33da10897154ab5a3da3fac59235a2fa7a5fde
Johannes Oster <johannes.oster@outlook.at>
2024-08-25 13:45:36 +0200

    Fix incorrect command in storage service

--- src/infrastructure/storageService/storageService.ts
+++ src/infrastructure/storageService/storageService.ts
@@ -2,7 +2,7 @@ import {getSignedUrl} from '@aws-sdk/s3-request-presigner';
 
 import {
   DeleteObjectCommand,
-  GetObjectAclCommand,
+  GetObjectCommand,
   ListObjectsCommand,
   PutObjectCommand,
   S3Client,
@@ -14,10 +14,12 @@ export const StorageService = () => {
   const bucket = config.get('awsS3.bucket');
   const client = new S3Client({region: config.get('awsS3.region')});
 
-  const getUrl = (path: string) => {
+  const getUrl = async (path: string) => {
     const params = {Bucket: bucket, Key: path};
-    const command = new GetObjectAclCommand(params);
-    return getSignedUrl(client, command, {expiresIn: 3600});
+    const command = new GetObjectCommand(params);
+    const url = await getSignedUrl(client, command, {expiresIn: 3600});
+    console.log(url);
+    return url;
   };
 
   type UploadParams = {path: string; contentType: string; data: Readable};
